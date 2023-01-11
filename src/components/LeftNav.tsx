f061f6a53ae465ad4546a768a53b6f54ed8e6b04
bobostudio <m18576493832@163.com>
2023-01-11 17:45:08 +0800

    feat: update LeftNav component

--- src/components/LeftNav.tsx
+++ src/components/LeftNav.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import { AiOutlineStar, AiFillStar, AiOutlineEllipsis, AiOutlineFolderOpen, AiFillFolder, AiOutlineTags, AiFillTags, AiOutlineDelete, AiFillDelete } from 'react-icons/ai'
+import { AiOutlineStar, AiFillStar, AiOutlineSwap, AiOutlineFolderOpen, AiFillFolder, AiOutlineTags, AiFillTags, AiOutlineDelete, AiFillDelete, AiOutlinePlus, AiOutlineDown } from 'react-icons/ai'
 import './LeftNav.css'
 
 export default function LeftNav() {
@@ -14,31 +14,33 @@ export default function LeftNav() {
         active: true,
         children: [
             {
-                index: 1,
-                name: '我的笔记'
+                key: 1,
+                name: '我的笔记',
+                active: false
             }
-        ]
+        ],
+        option: true
     }, {
         key: 'label',
         name: '标签',
-        active: false
+        active: false,
+        option: true
     }, {
         key: 'trash',
         name: '回收站',
         active: false
     }])
-    const changeNav = (data: any) => {
-
+    let [option, setOption] = useState(false)
+    const changeNav = (data: any, type: number) => {
         current.map((item: any) => {
-            item['active'] = false
-            if (item['key'] === data) {
-                item['active'] = true
+            if (type === 1) {
+                item['active'] = false
+                if (item['key'] === data) {
+                    item['active'] = true
+                }
             }
-
         });
-
         setCurrent([...current])
-
     }
     const showIcon = (key: string, active: boolean) => {
         switch (key) {
@@ -54,6 +56,9 @@ export default function LeftNav() {
                 break;
         }
     }
+    const showOption = (key: string) => {
+        //console.log(key)
+    }
     return (
         <div className='flex flex-col justify-center'>
             {
@@ -62,20 +67,34 @@ export default function LeftNav() {
                         {
                             index === 1 && <div className='flex flex-row  justify-between items-center ml-2 mt-6 mb-3'>
                                 <span className='text-xs text-708 hover:cursor-pointer'>个人笔记</span>
-                                <AiOutlineEllipsis size={20} color='#7084A4' className='hover:cursor-pointer' />
+                                <AiOutlineSwap size={14} color='#7084A4' className='hover:cursor-pointer' />
                             </div>
                         }
-                        <div className={item.active ? 'flex flex-row mb-2 items-center  hover:cursor-pointer p-2 active' : 'flex flex-row  mb-2 items-center  hover:cursor-pointer p-2'} key={item.key} onClick={() => {
-                            changeNav(item.key)
-                        }}>
+                        <div className={item.active ? 'flex flex-row mb-2 items-center  hover:cursor-pointer p-2 active' : 'flex flex-row  mb-2 items-center  hover:cursor-pointer p-2'} key={item.key}
+                            onMouseOver={() => {
+                                showOption(item.key)
+                            }}
+                            onClick={() => {
+                                changeNav(item.key, 1)
+                            }}>
                             {
                                 showIcon(item.key, item.active)
                             }
-                            <span className='text-sm text-extend '>{item.name}</span>
+                            {
+                                item.option ? <div className='flex justify-between items-center w-full'>
+                                    <span className='text-sm text-extend '>{item.name}</span>
+                                    <div id='option' className='flex flex-row justify-end'>
+                                        <span><AiOutlinePlus size={14} color='#7084A4' className='mr-2' /></span>
+                                        <span><AiOutlineDown size={14} color='#7084A4' /></span>
+                                    </div>
+                                </div> : <span className='text-sm text-extend '>{item.name}</span>
+                            }
                         </div>
                         {
                             item.children && item.children.map((item: any) => {
-                                return <div className='flex flex-row ml-10 mb-2 items-center'>
+                                return <div className='flex flex-row ml-10 mb-2 items-center' key={item.key} onClick={() => {
+                                    changeNav(item.key, 2)
+                                }}>
                                     <span className='text-sm text-extend hover:cursor-pointer'>我的笔记</span>
                                 </div>
                             })
