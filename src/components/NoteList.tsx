f1787aaeef703818c0e032d81addbc257be564bf
bobostudio <m18576493832@163.com>
2023-01-13 15:23:24 +0800

    fix: add react-app-env.d.ts for images

--- src/components/NoteList.tsx
+++ src/components/NoteList.tsx
@@ -1,13 +1,12 @@
-import { AiOutlineMenuFold } from 'react-icons/ai'
 import NoteItem from './NoteItem'
 import './NoteList.css'
 export default function NoteList() {
-    const data = [1, 2, 3, 4, 5, 6,7,8,9,10]
+    const data = [1, 2, 3, 4, 5, 6]
     return (
         <>
             <div className="item-container">
                 {
-                    data.map((item,index) => {
+                    data.map((item, index) => {
                         if (index === 0) {
                             return <NoteItem key={item} active />
                         } else {
